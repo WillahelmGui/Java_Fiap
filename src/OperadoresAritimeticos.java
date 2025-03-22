f6d7043dfcc1ac1a922132470dda9ad1cb135c90
WillahelmGui <ribeiro.guicosta@gmail.com>
2025-03-21 22:29:54 -0300

    Mudança no OperadoresAritimeticos

--- src/OperadoresAritimeticos.java
+++ src/OperadoresAritimeticos.java
@@ -1,13 +1,20 @@
+import java.util.Scanner;
+
 public class OperadoresAritimeticos {
 
     public static void main(String[] args) {
         double notasCp, notaChallenge, notaGs;
         double notaFinal = 0;
-        notasCp = (double) (Math.random() * 10);
-        notaChallenge = (double) (Math.random() * 10);
-        notaGs = (double) (Math.random() * 10);
-
-        double notas[] = new double[3];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira nota da cp");
+        notasCp = sc.nextDouble();
+        System.out.println("Insira nota do challenge");
+        notaChallenge = sc.nextDouble();
+        System.out.println("Insira nota da GS");
+        notaGs = (double) sc.nextDouble();
+        System.out.println("Insira seu nome");
+        String nomeAluno =  sc.nextLine() + sc.nextLine();
+        double[] notas = new double[3];
 
         notas[0] = notasCp;
         notas[1] = notaChallenge;
@@ -17,7 +24,7 @@ public class OperadoresAritimeticos {
             System.out.println(nota);
              notaFinal += nota;
         }
-        System.out.println("Média: " + notaFinal / notas.length);
+        System.out.println(nomeAluno + " sua média é: " + (notaFinal / notas.length));
 
     }
 }
