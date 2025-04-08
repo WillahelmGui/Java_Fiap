af3d08ff5d7cb7b142aa480f42c7c988d72eefc5
ribeiro.guicosta@gmail.com <labsfiap@fiap.com.br>
2025-04-07 21:35:09 -0300

    Creating "Cliente","ContaCorrente" and "Endereco" classes from UML (UML is in the fourth slide)

---
+++ src/aula0704/Cliente.java
@@ -0,0 +1,11 @@
+package aula0704;
+
+public class Cliente {
+    String nome;
+    String cpf;
+    Endereco endereco;
+
+    String retornarDados(){
+     return this.toString();
+    }
+}
---
+++ src/aula0704/Endereco.java
@@ -0,0 +1,13 @@
+package aula0704;
+
+public class Endereco {
+    String logradouro;
+    short numero;
+    String complemento;
+    String cep;
+
+
+    String retornarEndereco(){
+        return this.toString();
+    }
+}
